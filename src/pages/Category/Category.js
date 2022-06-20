fcaafa226cc86aa88afbd48b3fece46ca4b984f0
noufal@reontel.com <noufal@reontel.com>
2022-06-20 18:32:17 +0530

    Erp screen Design

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -61,30 +61,32 @@ function Category() {
 
           <div className="container mt-5 justify-content-center">
             <div className="row justify-content-center">
-              <div className="d-flex justify-content-start pb-3  border-bottom">
-                <div className="col-2 m-0 p-0">
-                  <h4>Category</h4>
+              <div className=" d-flex justify-content-evenly pb-3  border-bottom">
+                <div className="col-6 m-0 p-0 d-flex gap-4">
+                  <div>
+                    <h4>Category</h4>
+                  </div>
+                  <div className=" m-0 p-0 d-flex  gap-2">
+                    <Button
+                    //  appendIcon={}
+                    >
+                      New Category <AiOutlinePlus />
+                    </Button>
+                    {/* </div>
+                <div className="col-2 m-0 p-0"> */}
+                    <Button
+                    // appendIcon={}
+                    >
+                      Upload Via CSV <FiUpload />
+                    </Button>
+                  </div>
                 </div>
-                <div className=" col-2 m-0 p-0">
-                  <Button
-                  //  appendIcon={}
-                  >
-                    New Category <AiOutlinePlus />
-                  </Button>
-                </div>
-                <div className="col-2 m-0 p-0">
-                  <Button
-                  // appendIcon={}
-                  >
-                    Upload Via CSV <FiUpload />
-                  </Button>
-                </div>
-
                 <div className="d-flex justify-content-end col-6 m-0 p-0">
                   <Newcontact />
                 </div>
               </div>
             </div>
+
             <div className="row mt-5 justify-content-center">
               <div>
                 <Table
